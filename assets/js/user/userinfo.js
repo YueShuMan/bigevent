e4740b95d0c9b53b7fa5bbb1cef0c07a484c3d27
YueShuMan <1092116068@qq.com>
2020-08-23 09:39:37 +0800

    完成了个人中心基本资料修改

--- assets/js/user/userinfo.js
+++ assets/js/user/userinfo.js
@@ -1,3 +1,6 @@
+// 加载form模块
+var form = layui.form;
+
 // ----------------------------------- 数据回填 ----------------------------------
 // 定义渲染表单的函数
 function renderForm() {
@@ -7,10 +10,21 @@ function renderForm() {
         success: function (res) {
             // console.log(res)
             // 设置每个input的value值 (用户名、昵称、邮箱、id)
-            $('input[name=username').val(res.data.username);
-            $('input[name=nickname').val(res.data.nickname);
-            $('input[name=email').val(res.data.email);
-            $('input[name=id').val(res.data.id);
+            // $('input[name=username').val(res.data.username);
+            // $('input[name=nickname').val(res.data.nickname);
+            // $('input[name=email').val(res.data.email);
+            // $('input[name=id').val(res.data.id);
+
+            // 使用layui提供了的form.val()快速为表单赋值 (数据回填)
+            // form.val('表单的lay-filter属性值', { 回填的数据 });
+            // form.val('user', {
+            //     username: 'zs',
+            //     nickname: 'ls',
+            //     email: '15453232@163.com',
+            //     id: 333
+            // });
+
+            form.val('user', res.data);
         }
     });
 }
@@ -40,5 +54,16 @@ $('form').on('submit', function (e) {
                 window.parent.getUserInfo();
             }
         }
-    })
+    });
+});
+
+
+
+// ------------------------------- 重置表单 ------------------------------------
+// 点击重置按钮，默认会清空表单；但是我们希望恢复成原来的样子
+$('button[type=reset]').click(function (e) {
+    // 阻止表单的默认行为 (清空表单行为)
+    e.preventDefault();
+    // 恢复表单 和 刷新页面后看到的效果一样
+    renderForm();
 })
